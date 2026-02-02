c64dbf098a1535992fede60b307770386c14e7ae
disepciuc <daniel.isepciuc@gmail.com>
2026-02-02 09:42:10 +0200

    Fix service worker typing for Serwist

--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,6 @@
+/// <reference lib="webworker" />
 import { Serwist } from "serwist";
 import { defaultCache } from "@serwist/next/worker";
-
-declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
